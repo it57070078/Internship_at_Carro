6910965adb6163e0ea241d6a09897cf8c111b5e6
Patcharapon <msophon38@gmail.com>
2017-06-15 13:18:58 +0700

    Delete Project

--- testflux/src/js/app.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
- 
-// โหลด component หลักมา
-var DiscussionApp = require('./components/DiscussionApp.jsx');
- 
-// แสดงผล component หลัก ที่ #app
-ReactDOM.render(<DiscussionApp /> , 
-document.getElementById('app'));
